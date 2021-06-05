327f3717755eb611f26e7a1fbecc7102d52367af
Felipe Silva <felipe.dacs@gmail.com>
2021-06-05 02:04:05 -0300

    :tv: add aula 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
-import App from './App'
+import App, { calcularNovoSaldo } from './App'
 
 describe('Componente principal', () => {
   describe('Quando eu abro o app do banco', () => {
-    it('O nome é exibido', () => {
+    it('o nome é exibido', () => {
       render(<App />)
 
       expect(screen.getByText('ByteBank')).toBeInTheDocument()
     })
 
-    it('O saldo é exibido', () => {
+    it('o saldo é exibido', () => {
       render(<App />)
 
       expect(screen.getByText('Saldo:')).toBeInTheDocument()
     })
 
-    it('O botão de realizar a transação é exibido', () => {
+    it('o botão de realizar a transação é exibido', () => {
       render(<App />)
 
       expect(screen.getByText('Realizar operação')).toBeInTheDocument()
+    })
+  })
+
+  describe('Quando eu realizo uma transação', () => {
+    it('que é um saque, o valor vai diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 50
+      }
+      const novoSaldo = calcularNovoSaldo(valores, 150)
+
+      expect(novoSaldo).toBe(100)
     })
   })
 })
