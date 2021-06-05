19f568c7834b3b33d0d78aebc8533b4b0866257b
Felipe Silva <felipe.dacs@gmail.com>
2021-06-05 15:15:58 -0300

    :tv: pulei essa aula

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen as screenAlura } from '@testing-library/react'
 import Conta from './Conta'
+import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 
 describe('Componente de conta', () => {
   it('Exibir o saldo da conta como valor monetário', () => {
@@ -8,5 +10,17 @@ describe('Componente de conta', () => {
 
     const saldo = screen.getByTestId('saldo-conta')
     expect(saldo.textContent).toBe('R$ 1000')
+  })
+
+  it('Chama a função de realizar a transação, quando o botão é clicado', () => {
+    const funcaoRealizarTransacao = jest.fn()
+
+    render(<Conta
+      saldo={1000}
+      realizarTransacao={funcaoRealizarTransacao}
+    />)
+
+    userEvent.click(screen.getByText('Realizar operação'))
+    expect(funcaoRealizarTransacao).toHaveBeenCalled()
   })
 })
