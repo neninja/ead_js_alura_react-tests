cdf9fe672f3b861b0eb9c5b77acca49e52ce7644
Felipe Silva <felipe.dacs@gmail.com>
2021-06-05 01:27:05 -0300

    :tv: add aula 2

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,24 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('Componente principal', () => {
-  it('Mostrar o nome do banco', () => {
-    render(<App />)
+  describe('Quando eu abro o app do banco', () => {
+    it('O nome é exibido', () => {
+      render(<App />)
 
-    expect(screen.getByText('ByteBank')).toBeInTheDocument()
+      expect(screen.getByText('ByteBank')).toBeInTheDocument()
+    })
+
+    it('O saldo é exibido', () => {
+      render(<App />)
+
+      expect(screen.getByText('Saldo:')).toBeInTheDocument()
+    })
+
+    it('O botão de realizar a transação é exibido', () => {
+      render(<App />)
+
+      expect(screen.getByText('Realizar operação')).toBeInTheDocument()
+    })
   })
 })
+
